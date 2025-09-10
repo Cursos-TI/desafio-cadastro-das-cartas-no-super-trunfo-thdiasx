76cdd6d98873af274218a2e8a564c7f5356b04c2
thdiasx <thiago.h.dias2006@gmail.com>
2025-09-10 16:10:50 +0000

    atualizacao codigo aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,8 @@ int main() {
 
     int PontoTuristico1,PontoTuristico2,Populaçao1, Populacao2;
     float PIB1,PIB2,Area1,Area2;
+    float DensidadePopulacional1,DensidadePopulacional2;
+    float PIBpc1,PIBpc2;
     char Estado1,Estado2;
     char Cidade1[10],Cidade2[10],Codigo1[10],Codigo2[10] ;
 
@@ -33,6 +35,13 @@ int main() {
     scanf("%d %d", &PontoTuristico1, &PontoTuristico2);
     printf("\n");
 
+    //Declarando os valores de Densidade Populacional e PIB per capita
+
+    DensidadePopulacional1 = (float) Populaçao1 / Area1; 
+    DensidadePopulacional2 = (float) Populacao2 / Area2;
+    PIBpc1 = (float) (PIB1 * 1000000000.0) / Populaçao1;
+    PIBpc2 = (float) (PIB2 * 1000000000.0) / Populacao2;
+
     // Printando a primeira carta na tela
 
     printf("CARTA 1:\n");
@@ -40,9 +49,11 @@ int main() {
     printf("Codigo: %s \n", Codigo1);
     printf("Cidade: %s \n", Cidade1);
     printf("População: %d \n", Populaçao1);
-    printf("Area: %f \n",Area1);
-    printf("PIB: %f \n",PIB1);
+    printf("Area: %.2f km² \n",Area1);
+    printf("PIB: %.2f bilhoes de reais \n",PIB1);
     printf("Pontos Turisticos: %d \n",PontoTuristico1);
+    printf("Densidade Populacional: %.2f hab/km² \n",DensidadePopulacional1);
+    printf("PIB per capita: %.2f reais \n",PIBpc1);
     printf("\n");
 
     // Printando a segunda carta na tela
@@ -52,9 +63,11 @@ int main() {
     printf("Codigo: %s \n", Codigo2);
     printf("Cidade: %s \n", Cidade2);
     printf("População: %d \n", Populacao2);
-    printf("Area: %f \n", Area2);
-    printf("PIB: %f \n", PIB2);
+    printf("Area: %.2f km² \n", Area2);
+    printf("PIB: %.2f bilhoes de reais \n", PIB2);
     printf("Pontos Turisticos: %d \n", PontoTuristico2);
+    printf("Densidade Populacional: %.2f hab/km² \n",DensidadePopulacional2);
+    printf("PIB per capita: %.2f reais \n",PIBpc2);
 
     return 0;
 }
